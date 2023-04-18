8326f717ff2d1d22636498164f4351fcfc42e5bc
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-04-18 09:02:21 +0800

    Create App.java

---
+++ week_4/postcondition/App.java
@@ -0,0 +1,21 @@
+public class App {
+
+    static boolean iWontDrink = false;
+    static boolean iAmDrunk = false;
+
+    public static void main(String[] args) throws Exception {
+      
+        App myApp = new App();
+
+        myApp.goToParty();
+
+        // Postcondition
+        assert iAmDrunk = false: "You wont drink ever again!";
+    }
+
+
+    void goToParty(){
+        
+        iAmDrunk = true;
+    }
+}
