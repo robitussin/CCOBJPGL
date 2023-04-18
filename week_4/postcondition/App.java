5f66bb0fdfc2c5bfa1f94b8370725676b1a37c29
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-04-18 11:07:32 +0800

    Update App.java

--- week_4/postcondition/App.java
+++ week_4/postcondition/App.java
@@ -10,7 +10,7 @@ public class App {
         myApp.goToParty();
 
         // Postcondition
-        assert iAmDrunk = false: "You wont drink ever again!";
+        assert iAmDrunk == false: "You wont drink ever again!";
     }
 
 
