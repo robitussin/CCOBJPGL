9fa11eaebbd187a14a7e1eb17194e4763e892fdd
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-04-20 19:02:20 +0800

    Added event handler

---
+++ JavaFX/Event Handler/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+
+    public static void main(String[] args) {
+        
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));  
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
