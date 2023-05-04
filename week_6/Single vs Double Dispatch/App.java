517b6052a0779bdc3f2fcffbf51e41b22c8449cb
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-05-04 18:50:11 +0800

    Added RobotExplorer

--- week_6/Single vs Double Dispatch/App.java
+++ week_6/Single vs Double Dispatch/App.java
@@ -1,23 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/* Single vs Double Dispatch */ 
+
 public class App {
     public static void main(String[] args) throws Exception {
 
         Planet mars = new Mars();
 
         Explorer astronaut = new HumanExplorer();
+        Explorer rover = new RobotExplorer();
 
+        // ArrayList
         List<Explorer> explorerlist = new ArrayList<Explorer>();
         explorerlist.add(astronaut);
+        explorerlist.add(rover);
         
-        // Single Dispatch
+        //Single Dispatch
         for (Explorer explorer : explorerlist) {
             explorer.visit();
         }
 
         // Double dispatch does not work in Java
         astronaut.visit(mars);
-
     }
 }
