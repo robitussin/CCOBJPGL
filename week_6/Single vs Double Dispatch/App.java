39f5c85bf33caebbf96a9023761ececd017a4cac
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-05-04 08:59:12 +0800

    Changed java implementation of comparison of Single vs Double dispatch

--- week_6/Single vs Double Dispatch/App.java
+++ week_6/Single vs Double Dispatch/App.java
@@ -1,38 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/* 
-Single Dispatch vs Double Dispatch
-*/
-
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Animal dog = new Dog();
-        Animal cat = new Cat();
+        Planet mars = new Mars();
 
-        List<Animal> myAnimals = new ArrayList<Animal>();
+        Explorer astronaut = new HumanExplorer();
 
-        // Even if the cat and dog are very different classes, they share the same
-        // "Animal" superclass and therefore can be inserted in a single array. This is
-        // the advantage of polymorphism
-
-        myAnimals.add(dog);
-        myAnimals.add(cat);
-
-        for (Animal animal : myAnimals) {
-            animal.makeSound();
+        List<Explorer> explorerlist = new ArrayList<Explorer>();
+        explorerlist.add(astronaut);
+        
+        // Single Dispatch
+        for (Explorer explorer : explorerlist) {
+            explorer.visit();
         }
 
-        // Even if the dog and cat objects were both declared as type Animal,
-        // Single dispatch dynamically chose the correct method to invoke at runtime.
-        dog.makeSound();
-        cat.makeSound();
+        // Double dispatch does not work in Java
+        astronaut.visit(mars);
 
-        // This will result in an error if there is no makeSound() method that accepts
-        // an object of type Animal
-        // Java does not recognize which type of animal was passed
-        dog.makeSound(cat);
-        cat.makeSound(dog);
     }
 }
---
+++ week_6/Single vs Double Dispatch/Mars.java
@@ -0,0 +1,3 @@
+public class Mars implements Planet{
+    
+}
