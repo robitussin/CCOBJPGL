2123537a8c3e0348f5cb0d280b69870fd2f7a254
robitussin <elizerponiojr@gmail.com>
2023-05-22 18:59:24 +0800

    added sample java implementation of mvc

---
+++ week_9/Model-View-Controller/model/Fruit.java
@@ -0,0 +1,23 @@
+package model;
+
+class Fruit {
+
+    private String color;
+    private String taste;
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTaste() {
+        return this.taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+}
