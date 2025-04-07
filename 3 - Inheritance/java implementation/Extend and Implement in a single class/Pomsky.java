963cc53b0669e0e22df93dddce65a95bfe392b3f
robitussin <elizerponiojr@gmail.com>
2025-04-07 12:20:26 +0800

    Updated slides and examples

--- 3 - Inheritance/java implementation/Extend and Implement in a single class/Pomsky.java
+++ 3 - Inheritance/java implementation/Extend and Implement in a single class/Pomsky.java
@@ -1,5 +1,6 @@
 class Pomsky extends Husky implements Pomeranian {
 
+    @Override
     public void bark() {
         System.out.println("My pomsky is barking");
     }
--- 3 - Inheritance/java implementation/Multiple Inheritance using Interfaces/App.java
+++ 3 - Inheritance/java implementation/Multiple Inheritance using Interfaces/App.java
@@ -1,10 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Create object from pet class
         Pomsky myPomsky = new Pomsky();
 
-        myPomsky.bark();
+        myPomsky.bark(); // Output: Woof! Woof!
+        myPomsky.play(); // Output: Pomsky is playing!
+        myPomsky.dig(); // Output: Pomsky is digging!
         System.out.println("My pomsky has " + myPomsky.eyeColor + " eyes.");
         System.out.println("My pomsky is " + myPomsky.height + " tall.");
     }
